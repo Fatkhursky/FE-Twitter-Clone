5ee56c6438531c7eb8a2404bcdc2ce588253e7b6
Fatkhursky <fatkhurrosyad0@gmail.com>
2022-08-14 12:09:23 +0700

    add usemution

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,12 +5,16 @@ import '@/src/styles/add-tweet.scss'
 import '@/src/styles/index-2.scss'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
+import { ApolloProvider } from '@apollo/client'
+import client from '@/src/utilities/apollo'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <ApolloProvider client={client}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </ApolloProvider>
   )
 }
 export default MyApp
