bae9b1eea39b55ea61563a7e21da3bc1645bf7f6
oxwazz <muhammad.rahmahalim@gmail.com>
2022-08-28 16:49:35 +0700

    fix: error build

--- pages/Explore/index.tsx
+++ pages/Explore/index.tsx
@@ -1,34 +1,25 @@
-//@ts-nocheck
-import React, { useEffect, useState } from 'react'
-import { GET_TWEETS } from '@/src/requests/graphql'
 import { useQuery } from '@apollo/client'
-import { decodeToken } from 'react-jwt'
 import { DATAUSER_QUERY } from '@/src/requests/graphql'
-const index = () => {
-const [token, setToken] = useState()
-    useEffect (() => {
+import { useSession } from 'next-auth/react'
+const PageExplore = () => {
+  const { data: session } = useSession()
 
-        const item = localStorage.getItem('Bearer')
-        setToken(decodeToken(item))
+  const { loading, error, data } = useQuery(DATAUSER_QUERY, {
+    variables: {
+      where: {
+        id: 31,
+      },
+    },
+  })
+  if (loading) return 'Loading...'
+  if (error) return `Error! ${error.message}`
+  console.log(data)
 
-    }, [])
-
-    const { loading, error, data } = useQuery(DATAUSER_QUERY, {
-        variables: {
-            where: {
-                id: 31
-              },
-        },
-    });
-    if (loading) return 'Loading...';
-    if (error) return `Error! ${error.message}`;
-    console.log(data)
-    
   return (
     <div>
-        <button onClick={() => console.log(token.data.id)}>CLIK</button>
+      <button onClick={() => console.log(session?.id)}>CLIK</button>
     </div>
   )
 }
 
-export default index
+export default PageExplore
